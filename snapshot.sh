194616eedd0e8cf9cc040ae00af169588fe0e6d6
John Ford <john@johnford.info>
2012-07-17 15:49:52 -0700

    Store the device name given on the config.sh command line

--- snapshot.sh
+++ snapshot.sh
@@ -2,28 +2,12 @@
 
 . load-config.sh
 
-# Map the build system device name back to the one used against config.sh
-case $DEVICE in
-    generic)
-        pr_dev=emulator ;;
-    generic_x86)
-        pr_dev=emulator-x86 ;;
-    panda)
-        pr_dev=pandaboard ;;
-    maguro)
-        pr_dev=galaxy-nexus ;;
-    crespo)
-        pr_dev=nexus-s ;;
-    *)
-        pr_dev=$DEVICE ;;
-esac 
-
 b2g_root=$(cd `dirname $0` ; pwd)
 b2g_basename=$(basename $b2g_root)
 b2g_parent=$(dirname $b2g_root)
 branch=$(cd $b2g_root/.repo/manifests && git rev-parse --abbrev-ref HEAD)
 manifest_file=sources.xml
-output="B2G_${branch}_${pr_dev}.tar.gz"
+output="B2G_${branch}_${DEVICE_NAME}.tar.gz"
 
 if [ $OUT_DIR ] ; then
     if [ -d $b2g_root/$OUT_DIR ] ; then
@@ -37,7 +21,7 @@ if [ $GECKO_OBJDIR ] ; then
     fi
 fi
 
-test $pr_dev && test $branch &&
+test $DEVICE_NAME && test $branch &&
 echo Creating manifest &&
 $b2g_root/gonk-misc/add-revision.py $b2g_root/.repo/manifest.xml \
         --output $manifest_file --force --b2g-path $b2g_root --tags &&
@@ -46,7 +30,7 @@ nice tar zcf "$output" \
     -C $b2g_parent \
     --checkpoint=1000 \
     --checkpoint-action=dot \
-    --transform="s,^$b2g_basename,B2G_${branch}_${pr_dev}," \
+    --transform="s,^$b2g_basename,B2G_${branch}_${DEVICE_NAME}," \
     --exclude=".git" \
     --exclude=".hg" \
     --exclude="$b2g_basename/$output" \
