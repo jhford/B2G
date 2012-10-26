c14a8ed04b9217b62978edf706237096869d6eb1
John Ford <john@johnford.info>
2012-10-25 22:28:29 -0700

    split the source tree and device configuration into two scripts

--- config.sh
+++ config.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
+# This script understands how to create and sync a B2G source tree.
+# config.sh script used to also configure the .config file and even
+# further back also drove the blob setup
 
 REPO=./repo
-
-repo_sync() {
-	rm -rf .repo/manifest* &&
-	$REPO init -u $GITREPO -b $BRANCH -m $1.xml &&
-	$REPO sync
-	ret=$?
-	if [ "$GITREPO" = "$GIT_TEMP_REPO" ]; then
-		rm -rf $GIT_TEMP_REPO
-	fi
-	if [ $ret -ne 0 ]; then
-		echo Repo sync failed
-		exit -1
-	fi
-}
-
-case `uname` in
-"Darwin")
-	CORE_COUNT=`system_profiler SPHardwareDataType | grep "Cores:" | sed -e 's/[ a-zA-Z:]*\([0-9]*\)/\1/'`
-	;;
-"Linux")
-	CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
-	;;
-*)
-	echo Unsupported platform: `uname`
-	exit -1
-esac
 
 GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
 BRANCH=${BRANCH:-master}
@@ -34,7 +11,6 @@ BRANCH=${BRANCH:-master}
 GIT_TEMP_REPO="tmp_manifest_repo"
 if [ -n "$2" ]; then
 	GITREPO=$GIT_TEMP_REPO
-	GITBRANCH="master"
 	rm -rf $GITREPO &&
 	git init $GITREPO &&
 	cp $2 $GITREPO/$1.xml &&
@@ -42,82 +18,30 @@ if [ -n "$2" ]; then
 	git add $1.xml &&
 	git commit -m "manifest" &&
 	cd ..
+
+    if [ $? -ne 0 ] ; then
+        echo Could not set up temporary manifest repository
+        exit -1
+    fi
 fi
 
-echo MAKE_FLAGS=-j$((CORE_COUNT + 2)) > .tmp-config
-echo GECKO_OBJDIR=$PWD/objdir-gecko >> .tmp-config
-echo DEVICE_NAME=$1 >> .tmp-config
-
-case "$1" in
-"galaxy-s2")
-	echo DEVICE=galaxys2 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"galaxy-nexus")
-	echo DEVICE=maguro >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"optimus-l5")
-	echo DEVICE=m4 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-s")
-	echo DEVICE=crespo >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-s-4g")
-	echo DEVICE=crespo4g >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"otoro"|"unagi")
-	echo DEVICE=$1 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"pandaboard")
-	echo DEVICE=panda >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"emulator")
-	echo DEVICE=generic >> .tmp-config &&
-	echo LUNCH=full-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"emulator-x86")
-	echo DEVICE=generic_x86 >> .tmp-config &&
-	echo LUNCH=full_x86-eng >> .tmp-config &&
-	repo_sync emulator
-	;;
-
-*)
-	echo Usage: $0 \(device name\)
-	echo
-	echo Valid devices to configure are:
-	echo - galaxy-s2
-	echo - galaxy-nexus
-	echo - nexus-s
-	echo - nexus-s-4g
-	echo - otoro
-	echo - unagi
-	echo - pandaboard
-	echo - emulator
-	echo - emulator-x86
+rm -rf .repo/manifest* &&
+$REPO init -u $GITREPO -b $BRANCH -m $1.xml &&
+$REPO sync
+ret=$?
+if [ "$GITREPO" = "$GIT_TEMP_REPO" ]; then
+	rm -rf $GIT_TEMP_REPO
+fi
+if [ $ret -ne 0 ]; then
+	echo Repo sync failed
 	exit -1
-	;;
-esac
+fi
 
 if [ $? -ne 0 ]; then
-	echo Configuration failed
+	echo Source tree sync failed
 	exit -1
 fi
 
-mv .tmp-config .config
+# Start to actually configure the device
+./config-device.sh $1
 
-echo Run \|./build.sh\| to start building
